56e682bce158441b13491c60f3e793ffbe69a6df
cristian.corcoveanu <cristi.corcoveanu@gmail.com>
2016-07-28 11:15:01 +0300

    Playng examples on emag

--- spiders/spiders/spiders/emag.py
+++ spiders/spiders/spiders/emag.py
@@ -10,11 +10,12 @@ class EmagSpider(Spider):
 	filename        = 'emag.txt'
 
 	start_urls = [
-		'http://www.emag.ro/scaun-sitness-chief-200-textil-scaun-executiv-tapitat-cu-stofa-din-poliester-crem-0982/pd/DXTNVMBBM/?ref=hp_prod_widget_live_1_2&utm_source=receng_hp_prod_widget_live_1_v1&utm_medium=site&utm_content=recent_buy_std&utm_campaign=wd_ce_se_cumpara_acum_emag_hp'
+		'http://www.emag.ro/all-departments?ref=hdr_mm_14'
 	]
 
 	def parse(self, response):
-		self.cssSelectorParse(response)
+		for item in response.xpath('//div[@id="department-expanded"]/ul/li/ul/li/a/@href'):
+			print item.extract()
 
 	def cssSelectorParse(self, response):
 		item = EmagItem()
@@ -29,3 +30,7 @@ class EmagSpider(Spider):
 		with open(filename, 'wb') as f:
 			for text in header:
 				f.write(text.encode('utf-8'))
+
+	def testParse(self, response):
+		for item in response.xpath('//div[@id="department-expanded"]/ul/li/ul/li/a/@href'):
+			print item.extract()
