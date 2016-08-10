25ab25a52e8f620230f79225d80d139fa2711aed
Cristian Corcoveanu <cristian.corcoveanu@ebs.msft>
2016-08-10 10:16:34 +0300

    Test crawler with 1 second delay

--- spiders/spiders/settings.py
+++ spiders/spiders/settings.py
@@ -19,12 +19,12 @@ NEWSPIDER_MODULE = 'spiders.spiders'
 USER_AGENT = 'Staging Crawl - product_search_engine_web2'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS=32
+CONCURRENT_REQUESTS=16
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY=0.3
+DOWNLOAD_DELAY=1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN=16
 #CONCURRENT_REQUESTS_PER_IP=16
